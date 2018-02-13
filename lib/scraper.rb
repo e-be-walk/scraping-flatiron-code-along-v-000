18b86ddcf279e14dff4c7dbb5a305f9662f5532c
Erin Walker <erinb0317@hotmail.com>
2018-02-13 02:53:40 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,6 +8,11 @@ class Scraper
 
   def  get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+  end 
+
+  def get_courses 
+    self.get_page.css(".post")
+  end
 
     doc.css(".post").each do |post|
       course = Course.new
